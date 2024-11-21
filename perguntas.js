04a042f513845f9db365d9e28e673435d1a3efaf
3C-MORENO <00001083977684sp@al.educacao.sp.gov.br>
2024-11-21 13:09:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'O que é o DNA?',
+    'O DNA é a molécula que contém informações genéticas dos seres vivos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz pé em Inglês?',
+    'Pé em ingles é FOOT (FUTI)'
+)
